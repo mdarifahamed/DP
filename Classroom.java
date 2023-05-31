1066ace0d4929850274363ceb38fac49234abc71
Arif <arifonly4@gmail.com>
2023-05-31 17:36:08 +0530

    DP

---
+++ Classroom.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Classroom{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Float rad = sc.nextFloat(); // radius
+        Float area = 3.14f*rad*rad;
+        System.out.println(area);
+    }
+}
