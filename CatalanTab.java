220abca6021fb1e57afecb3a0839d3185a08bb18
Arif <arifonly4@gmail.com>
2023-06-26 08:30:21 +0530

    DP

---
+++ CatalanTab.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class CatalanTab {
+    public static int catalanTab(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            for(int j=0; j<i; j++){
+                dp[i] += dp[j] * dp[i-j-1]; // ci = cj * ci-j-1
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        System.out.println(catalanTab(5));
+    }
+}
